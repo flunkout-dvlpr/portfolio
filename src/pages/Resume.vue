0f06aefcad2d98d7f06f0630a368531f4250890f
flunkout-dvlpr <info@juliogonzalez.space>
2020-07-08 00:20:36 -0500

    Create Resume Page based on quasar timeline componenet

---
+++ src/pages/Resume.vue
@@ -0,0 +1,118 @@
+<template>
+<div class="q-px-lg q-pb-md">
+  <q-timeline color="secondary" layout="loose">
+
+    <q-timeline-entry heading>
+      July, 1996
+    </q-timeline-entry>
+
+    <q-timeline-entry
+      title="A bit about me"
+      subtitle="July 22, 1996"
+      side="left"
+    >
+      <div>
+        I was born
+      </div>
+    </q-timeline-entry>
+
+    <q-timeline-entry heading>
+      June, 2014
+    </q-timeline-entry>
+
+    <q-timeline-entry
+      title="High School"
+      subtitle="June 04, 2014"
+      icon="delete"
+      side="right"
+    >
+      <div>
+        I graduated HS
+      </div>
+    </q-timeline-entry>
+
+    <q-timeline-entry
+      title="College"
+      subtitle="2014-2016"
+      icon="delete"
+      side="left"
+    >
+      <div>
+        Texas A&M Computer Science
+      </div>
+    </q-timeline-entry>
+
+    <q-timeline-entry heading>
+      January, 2017
+    </q-timeline-entry>
+
+    <q-timeline-entry
+      title="Dropp Out"
+      subtitle="January 06, 2017"
+      avatar="https://cdn.quasar.dev/img/avatar2.jpg"
+      side="right"
+    >
+      <div>
+        I fucked up...I thought I did
+      </div>
+    </q-timeline-entry>
+
+    <q-timeline-entry
+      title="Depression or somthing"
+      subtitle="January 06, 2017"
+      avatar="https://cdn.quasar.dev/img/avatar2.jpg"
+      side="left"
+    >
+      <div>
+        I fucked up...I thought I did
+      </div>
+    </q-timeline-entry>
+
+    <q-timeline-entry heading>
+      January, 2018
+    </q-timeline-entry>
+
+    <q-timeline-entry
+      title="A New Start At Start Up"
+      subtitle="January 02, 2018"
+      avatar="https://cdn.quasar.dev/img/avatar2.jpg"
+    >
+      <div>
+        Software Developer at an O&G Startup
+      </div>
+    </q-timeline-entry>
+
+    <q-timeline-entry heading>
+      2020
+    </q-timeline-entry>
+
+    <q-timeline-entry
+      title="It's Not Easy To Say Goodby"
+      subtitle="March 16, 2020"
+      avatar="https://cdn.quasar.dev/img/avatar2.jpg"
+      side="left"
+    >
+      <div>
+        Comfort is comfy but discomfort is even better
+      </div>
+    </q-timeline-entry>
+
+    <q-timeline-entry
+      title="Starting from scratch"
+      subtitle="May, 2020"
+      avatar="https://cdn.quasar.dev/img/avatar2.jpg"
+      side="right"
+    >
+      <div>
+        Clean Code LLC
+      </div>
+    </q-timeline-entry>
+  </q-timeline>
+</div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+</style>
